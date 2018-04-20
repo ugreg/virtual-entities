ec0ce51f3cddbe2785b19cbd9c3344b0693ad121
Greg Degruy <gregory.degruy@gmail.com>
2018-04-20 16:10:32 -0700

    + super basic api code on server with ❤

--- README.md
+++ README.md
@@ -5,5 +5,8 @@
 Dynamics 365 Virtual Entity
 
 # References
-Modified from [Building a Basic RestFul API in Python
+- Modified from [Building a Basic RestFul API in Python
 ](https://www.codementor.io/sagaragarwal94/building-a-basic-restful-api-in-python-58k02xsiq) by [Sagar Chand Agarwal](https://github.com/sagaragarwal94)
+
+- [SQLite](https://sqlite.org/download.html)
+  -  Binaries for the SQLite DLL, and for the SQLite Command Shell. Unzip the contents of the files in a folder then add the folder to your PATH variable
---
+++ server.py
@@ -0,0 +1,36 @@
+from flask import flask
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from json import dumps
+from flask.ext.jsonpify import jsonify
+
+db_connection = create_engine('sqlite://chinook.db')
+
+app = Flask(__name__)
+api = Api(app)
+
+class Employees(Resource):
+    def get(self):
+        conn = db_connection.connect()
+        query = conn.execute("select * from exployees")
+        return { 'employees': [i[0] for i in query.cursor.fetchall()] } # fetch first column in employees table
+
+class Tracks(object):
+    def get(self):
+        conn = db_connection.connect()
+        query = conn.execute("select trackid, name, composer, unitprice from tracks")
+        result = { 'data': [dict( zip( tuple(query.keys(), i))) for i in query.cursor] }
+        return jsonify(result)
+
+class Employee_Name(Resource):
+    def get(self, employee_id):
+        conn = db_connection.connect()
+        query = conn.execute("select * from employees where EmployeeId = %d", %int(employee_id))
+        result = { 'data': [dict( zip( tuple( query.keys(), i))) for i in query.cursor] }
+
+api.add_resource(Employee, '/employees')
+api.add_resource(Tracks, '/employees')
+api.add_resource(Employee_Name, '/employees/<employee_id>')
+
+if __name__ == '__main__':
+    main.run(port = '1337')
