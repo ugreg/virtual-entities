31a01ae2c363efcc0ec088e4920ac5cefb06a8da
Greg Degruy <gregory.degruy@gmail.com>
2018-04-25 12:27:36 -0700

    + web orders model calss

---
+++ csharp/VirutalEntityDemo/Models/WebOrders.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VirutalEntityDemo.Models
+{
+    public class WebOrders
+    {
+        private Guid _id;
+        private DateTime _dateTime;
+        private Guid _customerId;
+        private float _total;
+        private float _tax;
+
+        public WebOrders() { }
+
+        public Guid Id { get { return _id; } }
+
+        public DateTime DateTime
+        {
+            get { return _dateTime; }
+            set { _dateTime = value; }
+        }
+
+        public Guid CusomterId { get { return _customerId; } }
+
+        public float Total
+        {
+            get { return _total; }
+            set { _total = value; }
+        }
+
+        public float Tax
+        {
+            get { return _tax; }
+            set { _tax = value; }
+        }
+    }
+}
