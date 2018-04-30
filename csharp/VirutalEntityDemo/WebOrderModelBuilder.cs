2b23842e4cb3214e0d2c685fda831b0a2db8f416
Greg Degruy <gregory.degruy@gmail.com>
2018-04-30 01:53:48 -0700

    + weborder model builder with allowed odata commands

---
+++ csharp/VirutalEntityDemo/WebOrderModelBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.OData.Edm;
+
+using VirutalEntityDemo.Models;
+
+namespace VirutalEntityDemo
+{
+    public class WebOrderModelBuilder
+    {
+        public IEdmModel GetEdmModel(IServiceProvider serviceProvider)
+        {
+            var builder = new ODataConventionModelBuilder(serviceProvider);
+
+            builder.EntitySet<WebOrder>(nameof(WebOrder))
+                .EntityType
+                .Filter()   // Allow $filter
+                .Count()    // Allow $count
+                .Expand()   // Allow $expand 
+                .OrderBy()  // Allow $orderby 
+                .Page()     // Allow $top and $skip
+                .Select();  // Allow $select 
+
+            return builder.GetEdmModel();
+        }
+    }
+}
