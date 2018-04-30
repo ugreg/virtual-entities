53f904f3175296fe87a3d53509e6615e39d2d08f
Greg Degruy <gregory.degruy@gmail.com>
2018-04-29 19:56:50 -0700

    ~ model with shorthand and note on sqlite types

--- csharp/VirutalEntityDemo/Models/WebOrder.cs
+++ csharp/VirutalEntityDemo/Models/WebOrder.cs
@@ -5,11 +5,13 @@ namespace VirutalEntityDemo.Models
 {
     public class WebOrder
     {
-        private Guid _id;
-        private DateTime _dateTime;
-        private Guid _customerId;
-        private float _total;
-        private float _tax;
+        private Guid _id;           // sqlite type BLOB
+        private DateTime _dateTime; // sqlite type TEXT
+        private Guid _customerId;   // sqlite type BLOB
+        private float _total;       // sqlite type REAL
+        private float _tax;         // sqlite type REAL
+        private string _status;     // sqlite type TEXT
+        private string _name;       // sqlite type TEXT
 
         public WebOrder(Guid id, DateTime dateTime, Guid customerId, float total, float tax)
         {
@@ -21,30 +23,23 @@ namespace VirutalEntityDemo.Models
         }
 
         [Key]
-        public Guid Id { get; private set; }
-
-        [Required]
-        public DateTime DateTime
+        public Guid Id
         {
-            get { return _dateTime; }
-            set { _dateTime = value; }
+            get { return _id; }
+            private set { _id = value; }
         }
 
         [Required]
-        public Guid CusomterId { get; private set; }
-
+        public DateTime DateTime { get => _dateTime;   set => _dateTime = value; }
         [Required]
-        public float Total
-        {
-            get { return _total; }
-            set { _total = value; }
-        }
-
+        public Guid CusomterId   { get => _customerId; set => _customerId = value; }
         [Required]
-        public float Tax
-        {
-            get { return _tax; }
-            set { _tax = value; }
-        }
+        public float Total       { get => _total;      set => _total = value; }
+        [Required]
+        public float Tax         { get => _tax;        set => _tax = value; }
+        [Required]
+        public string Status     { get => _status;     set => _status = value; }
+        [Required]
+        public string Name       { get => _name;       set => _name = value; }        
     }
 }
